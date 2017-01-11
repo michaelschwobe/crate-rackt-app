d2536c06658c88b52a12c755ac96127328b811fa
Michael Schwobe <mschwobe@gmail.com>
2017-01-10 18:58:34 -0600

    Update packages, Add new lint rule override, Fix new lint errors

--- src/components/Icon/index.stories.js
+++ src/components/Icon/index.stories.js
@@ -9,19 +9,19 @@ import Icon, { icons } from './';
 storiesOf('Icon', module)
   .add('default', () => (
     <div>
-      {Object.keys(icons).map((icon, i) => <Icon key={i} type={icon} />)}
+      {Object.keys(icons).map(icon => <Icon key={icon} type={icon} />)}
     </div>
   ))
   .add('currentColor', () => (
     <div style={{ color: 'green' }}>
-      {Object.keys(icons).map((icon, i) => <Icon key={i} type={icon} />)}
+      {Object.keys(icons).map(icon => <Icon key={icon} type={icon} />)}
     </div>
   ))
   .add('vertical alignment', () => (
     <div>
-      <div style={{ fontSize: '1em' }}>X{Object.keys(icons).map((icon, i) => <Icon key={i} type={icon} />)}X</div>
-      <div style={{ fontSize: '2em' }}>X{Object.keys(icons).map((icon, i) => <Icon key={i} type={icon} />)}X</div>
-      <div style={{ fontSize: '4em' }}>X{Object.keys(icons).map((icon, i) => <Icon key={i} type={icon} />)}X</div>
-      <div style={{ fontSize: '8em' }}>X{Object.keys(icons).map((icon, i) => <Icon key={i} type={icon} />)}X</div>
+      <div style={{ fontSize: '1em' }}>X{Object.keys(icons).map(icon => <Icon key={icon} type={icon} />)}X</div>
+      <div style={{ fontSize: '2em' }}>X{Object.keys(icons).map(icon => <Icon key={icon} type={icon} />)}X</div>
+      <div style={{ fontSize: '4em' }}>X{Object.keys(icons).map(icon => <Icon key={icon} type={icon} />)}X</div>
+      <div style={{ fontSize: '8em' }}>X{Object.keys(icons).map(icon => <Icon key={icon} type={icon} />)}X</div>
     </div>
   ));
