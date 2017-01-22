993289c086dc524b61bc14ab9c7b351afafaddda
Michael Schwobe <mschwobe@gmail.com>
2017-01-22 17:04:30 -0600

    Added CheckboxRadio component

---
+++ src/components/CheckboxRadio/index.js
@@ -0,0 +1,94 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+// Utils.
+import { slugify } from '../../utils';
+
+// Siblings.
+import styles from './index.css';
+
+//--------------------------------------------------------------------------------------------------
+
+export const types = ['checkbox', 'radio'];
+
+//--------------------------------------------------------------------------------------------------
+
+const CheckboxRadio = (props) => {
+  const {
+    className,
+    classNameControl,
+    classNameLabel,
+    type,
+    id,
+    name,
+    text,
+    reverse,
+    wide,
+    required,
+    readOnly,
+    disabled,
+    ...otherProps
+  } = props;
+
+  const classesRoot = classNames(styles.root, {
+    [styles.normal]: !reverse,
+    [styles.reverse]: reverse,
+    [styles.wide]: wide,
+    [styles.required]: required,
+    [styles.readonly]: readOnly,
+    [styles.disabled]: disabled,
+  }, className);
+  const classesInput = styles.input;
+  const classesControl = classNames(styles.control, styles[`${type}-control`], classNameControl);
+  const classesLabel = classNames(styles.text, classNameLabel);
+
+  const nameSlug = slugify(name);
+  const idSlug = id ? slugify(id) : nameSlug;
+
+  return (
+    <label className={classesRoot} htmlFor={idSlug}>
+      <input
+        className={classesInput}
+        type={type}
+        id={idSlug}
+        name={nameSlug}
+        required={required}
+        disabled={disabled || readOnly}
+        {...otherProps}
+      />
+      {' '}
+      <span className={classesControl} />
+      {' '}
+      <span className={classesLabel}>{text}</span>
+    </label>
+  );
+};
+
+CheckboxRadio.propTypes = {
+  className: PropTypes.string,
+  classNameControl: PropTypes.string,
+  classNameLabel: PropTypes.string,
+  type: PropTypes.oneOf(types).isRequired,
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+  wide: PropTypes.bool,
+  required: PropTypes.bool,
+  readOnly: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+
+CheckboxRadio.defaultProps = {
+  className: null,
+  classNameControl: null,
+  classNameLabel: null,
+  id: null,
+  reverse: false,
+  wide: false,
+  required: false,
+  readOnly: false,
+  disabled: false,
+};
+
+export default CheckboxRadio;
---
+++ src/components/CheckboxRadio/index.stories.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+
+// Siblings.
+import CheckboxRadio from './';
+
+//--------------------------------------------------------------------------------------------------
+
+storiesOf('CheckboxRadio', module)
+  .add('checkbox', () => (
+    <div>
+      <div><CheckboxRadio type="checkbox" name="a0" text="a0" /> ← default </div>
+      <div><CheckboxRadio type="checkbox" name="a1" text="a1" readOnly /> ← readOnly </div>
+      <div><CheckboxRadio type="checkbox" name="a2" text="a2" disabled /> ← disabled </div>
+      <div><CheckboxRadio type="checkbox" name="a3" text="a3" required /> ← required </div>
+      <br />
+      <div><CheckboxRadio type="checkbox" name="b1" text="b1" defaultChecked /> ← default, defaultChecked </div>
+      <div><CheckboxRadio type="checkbox" name="b2" text="b2" defaultChecked readOnly /> ← readOnly, defaultChecked </div>
+      <div><CheckboxRadio type="checkbox" name="b3" text="b3" defaultChecked disabled /> ← disabled, defaultChecked </div>
+      <div><CheckboxRadio type="checkbox" name="b4" text="b4" defaultChecked required /> ← required, defaultChecked </div>
+      <br />
+      <div>
+        <CheckboxRadio type="checkbox" name="c" id="c1" text="c1" />{' '}
+        <CheckboxRadio type="checkbox" name="c" id="c2" text="c2" defaultChecked />{' '}
+        <CheckboxRadio type="checkbox" name="c" id="c3" text="c3" />{' '}
+        <CheckboxRadio type="checkbox" name="c" id="c4" text="c4" /> ← inline
+      </div>
+    </div>
+  ))
+  .add('radio', () => (
+    <div>
+      <div><CheckboxRadio type="radio" name="a0" text="a0" /> ← default </div>
+      <div><CheckboxRadio type="radio" name="a1" text="a1" readOnly /> ← readOnly </div>
+      <div><CheckboxRadio type="radio" name="a2" text="a2" disabled /> ← disabled </div>
+      <div><CheckboxRadio type="radio" name="a3" text="a3" required /> ← required </div>
+      <br />
+      <div><CheckboxRadio type="radio" name="b1" text="b1" defaultChecked /> ← default, defaultChecked </div>
+      <div><CheckboxRadio type="radio" name="b2" text="b2" defaultChecked readOnly /> ← readOnly, defaultChecked </div>
+      <div><CheckboxRadio type="radio" name="b3" text="b3" defaultChecked disabled /> ← disabled, defaultChecked </div>
+      <div><CheckboxRadio type="radio" name="b4" text="b4" defaultChecked required /> ← required, defaultChecked </div>
+      <br />
+      <div>
+        <CheckboxRadio type="radio" name="c" id="c1" text="c1" />{' '}
+        <CheckboxRadio type="radio" name="c" id="c2" text="c2" defaultChecked />{' '}
+        <CheckboxRadio type="radio" name="c" id="c3" text="c3" />{' '}
+        <CheckboxRadio type="radio" name="c" id="c4" text="c4" />
+      </div>
+    </div>
+  ))
+  .add('reverse', () => (
+    <div>
+      <div><CheckboxRadio type="checkbox" name="a0" text="a0" reverse /> ← default </div>
+      <div><CheckboxRadio type="checkbox" name="a1" text="a1" reverse readOnly /> ← readOnly </div>
+      <div><CheckboxRadio type="checkbox" name="a2" text="a2" reverse disabled /> ← disabled </div>
+      <div><CheckboxRadio type="checkbox" name="a3" text="a3" reverse required /> ← required </div>
+      <br />
+      <div><CheckboxRadio type="checkbox" name="b1" text="b1" reverse defaultChecked /> ← default, defaultChecked </div>
+      <div><CheckboxRadio type="checkbox" name="b2" text="b2" reverse defaultChecked readOnly /> ← readOnly, defaultChecked </div>
+      <div><CheckboxRadio type="checkbox" name="b3" text="b3" reverse defaultChecked disabled /> ← disabled, defaultChecked </div>
+      <div><CheckboxRadio type="checkbox" name="b4" text="b4" reverse defaultChecked required /> ← required, defaultChecked </div>
+      <br />
+      <div>
+        <CheckboxRadio type="checkbox" name="c" id="c1" text="c1" reverse />{' '}
+        <CheckboxRadio type="checkbox" name="c" id="c2" text="c2" reverse defaultChecked />{' '}
+        <CheckboxRadio type="checkbox" name="c" id="c3" text="c3" reverse />{' '}
+        <CheckboxRadio type="checkbox" name="c" id="c4" text="c4" reverse /> ← inline
+      </div>
+    </div>
+  ));
